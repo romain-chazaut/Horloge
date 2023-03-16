fec9758ba7f671619596c74fdf4db88795a3e75d
romain-chazaut <romain.chazaut@laplateforme.io>
2023-03-16 16:12:44 +0100

    2e commit

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import time
 
 def afficher_heures():
     # Définir l'heure de départ à 16h30
-    heure_depart = (16, 30, 0)
-
+    heure_depart = (16, 30, 00)
+    heure_alarme = (16, 31, 00)
+   
     while True:
         # Afficher l'heure
         heures, minutes, secondes = heure_depart
         print(f"{heures:02d}:{minutes:02d}:{secondes:02d}")
 
-        # Augmenter les secondes
+        if heure_depart == heure_alarme:
+            print("C'est l'heure")
+            break
         heures, minutes, secondes = heure_depart
         secondes += 1
 
@@ -27,11 +30,11 @@ def afficher_heures():
                 if heures == 24:
                     heures = 0
 
-        # Mettre à jour l'heure de départ
-        heure_depart = (heures, minutes, secondes)
-
-        # Attendre une seconde avant la prochaine mise à jour
+                heure_depart = (heures, minutes, secondes)
+      
         time.sleep(1)
 
 # Appel de la fonction pour afficher l'heure à partir de 16h30
 afficher_heures()
+
+
